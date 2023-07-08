d22bfce337154af0b36c7954ba6beeddb1ab2848
Wanangwa <Martinnyirenda8@gmail.com>
2023-07-08 11:36:03 +0000

    Loading projects from firebase

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyARJjZtDOcQSQAJMmp4ZzpQAegLuyZE5Q0",
@@ -12,3 +14,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
+export const storage = getStorage(app)
