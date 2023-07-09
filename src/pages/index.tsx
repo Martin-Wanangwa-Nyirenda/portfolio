50e0a2ded05c123fb5b4b7affab679a6bba844ec
Wanangwa <Martinnyirenda8@gmail.com>
2023-07-09 17:45:25 +0000

    Added autoscroll to contacts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,23 @@ import Technologies from "./components/Technologies";
 import Projects from "./components/Projects";
 import Contacts from "./components/Contact";
 import Head from "next/head";
+import { useRef } from "react";
 
 export default function Home() {
+  const sectionRef = useRef<HTMLParagraphElement>(null);
+
+  const handleButtonClick = () => {
+    if (sectionRef.current) {
+      sectionRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
     <>
       <Head>
         <title>Martin Nyirenda - Portfolio</title>
       </Head>
       <main className="bg-[url('/image3.png')] bg-repeat-y bg-cover flex flex-col items-center justify-center w-screen min-h-screen">
-        <div className="container max-w-5xl mx-auto">
+        <div className="container max-w-7xl mx-auto">
           <section className="flex flex-col justify-center items-center mt-8 md:mt-24">
             <h1 className="text-2xl font-bold md:text-4xl lg:text-4xl  text-center mt-16 w-3/6 bg-gradient-to-r from-zinc-50 to-purple-400 bg-clip-text text-transparent">
               Hi, I&apos;m Martin
@@ -19,8 +27,11 @@ export default function Home() {
             <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-6xl font-bold text-center mt-8 xl:w-3/6 md:w-3/4 sm:w-3/4 lg:w-3/6 bg-gradient-to-br from-zinc-300 to-purple-500 bg-clip-text text-transparent">
               I love coding and solving problems with my skills.
             </h1>
-            <button className="border-zinc-50 mt-20 border-2 py-3 px-4 font-bold rounded-lg hover:bg-sky-500 text-sm md:text-lg lg:text-lg">
-              Find out more
+            <button
+              onClick={() => handleButtonClick()}
+              className="border-zinc-50 mt-20 border-2 py-3 px-4 font-bold rounded-lg hover:bg-sky-500 text-sm md:text-lg lg:text-lg"
+            >
+              Get in touch
             </button>
           </section>
           <div className="flex justify-between items-center mt-24">
@@ -33,14 +44,17 @@ export default function Home() {
           <Projects />
           <div className="flex mt-24 items-center">
             <div className="hidden md:block border-t-4 mr-8 w-full"></div>
-            <p className="text-zinc-50 md:w-96 italic text-2xl md:text-4xl lg:text-4xl text-center mx-auto">
+            <p className="text-zinc-50 md:w-96 italic text-2xl md:text-4xl lg:text-4xl  xl:w-3/4 text-center mx-auto">
               you have seen my work, here is what is in my toolbox.
             </p>
           </div>
           <Technologies />
           <div className="flex mt-16 items-center">
             <div className="hidden md:block border-t-4 mr-8 w-full"></div>
-            <p className="text-zinc-50 md:w-96 italic text-xl sm:text-2xl md:text-4xl lg:text-4xl text-center">
+            <p
+              ref={sectionRef}
+              className="text-zinc-50 md:w-96 italic text-xl sm:text-2xl md:text-4xl lg:text-4xl xl:w-3/4 text-center"
+            >
               you have seen my work and toolbox. Here, is how you can get in
               touch with me.
             </p>
